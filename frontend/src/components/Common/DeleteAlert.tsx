52b3fae04d22b33573124455eb6e78e818d7fbf1
Yiming Chen <tiandi1xc@gmail.com>
2025-02-06 23:13:25 +0100

    remove unused login mechanism

--- frontend/src/components/Common/DeleteAlert.tsx
+++ frontend/src/components/Common/DeleteAlert.tsx
@@ -11,7 +11,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query"
 import React from "react"
 import { useForm } from "react-hook-form"
 
-import { ItemsService, UsersService } from "../../client"
+import { ItemsService } from "../../client"
 import useCustomToast from "../../hooks/useCustomToast"
 
 interface DeleteProps {
@@ -33,8 +33,6 @@ const Delete = ({ type, id, isOpen, onClose }: DeleteProps) => {
   const deleteEntity = async (id: string) => {
     if (type === "Item") {
       await ItemsService.deleteItem({ id: id })
-    } else if (type === "User") {
-      await UsersService.deleteUser({ userId: id })
     } else {
       throw new Error(`Unexpected type: ${type}`)
     }
