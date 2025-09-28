1f7523812a43a69bacd0aaa95c368cfb2e3bb967
Yiming Chen <tiandi1xc@gmail.com>
2025-09-28 17:22:21 +0800

    replace icon with placeholder and update screenshots

--- code/frontend/src/components/Common/UserMenu.tsx
+++ code/frontend/src/components/Common/UserMenu.tsx
@@ -20,8 +20,7 @@ const UserMenu = () => {
           <MenuButton
             as={IconButton}
             aria-label="Options"
-            icon={<FaUserAstronaut color="white" fontSize="18px" />}
-            bg="ui.main"
+            bg="#0606FC"
             isRound
             data-testid="user-menu"
           />
